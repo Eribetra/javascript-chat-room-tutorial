8073efacea03eddfd123b307726b1484aa6b484c
Eribetra <ericlaube.dutra@gmail.com>
2019-05-31 17:02:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -119,8 +119,7 @@ function updateMembersDOM() {
 function createMessageElement(text, member) {
   const el = document.createElement('div');
   el.appendChild(createMemberElement(member));
-      el.appendChild(document.createTextNode("at ", time)); 
-      el.appendChild(document.createTextNode(time)); 
+      el.appendChild(document.createTextNode(("at ", time))); 
         el.appendChild(document.createTextNode(": ")); 
   el.appendChild(document.createTextNode(text));
   el.className = 'message';
